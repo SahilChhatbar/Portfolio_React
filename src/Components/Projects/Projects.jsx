1e315426bd7ee30376776eeea754856b4902389e
SahilChhatbar <sahilchhatbar7@gmail.com>
2024-11-05 10:06:17 +0530

    ewc

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,12 +6,13 @@ const Projects = () => {
     <div id="Projects" className="p-10 md:p-24 md:pt-0 text-white md:-mt-20 -mt-8">
     <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
     <div className="py-12 px-8 flex flex-wrap gap-8 justify-center mt-0 md:mt-10">
-      <ProjectCard 
-        title="CineScope" 
-        main="A movie information website that contains movie lists, such as popular, upcoming, and top rated. It utilized real-time data using API calls. Also contains movie details page complete with a unique chatbot." 
-        image="https://i.postimg.cc/jSYyYNHn/cine.jpg" 
-        demoLink="https://cine-scope-phi.vercel.app/"
-        githubLink="https://github.com/SahilChhatbar/CineScope-ReactJs"
+      
+    <ProjectCard 
+        title="SamacharSetu" 
+        main="A news website with a translation feature, completely responsive. Developed using React and Bootstrap." 
+        image="https://i.postimg.cc/tCc51Yxh/Screenshot-674.png" 
+        demoLink="https://samacharsetu.vercel.app/"
+        githubLink="https://github.com/SahilChhatbar/SamacharSetu"
       />
       <ProjectCard 
         title="NearBees" 
@@ -20,7 +21,13 @@ const Projects = () => {
         demoLink="https://imgur.com/a/KQZGUDr"
         githubLink="https://github.com/SahilChhatbar/NearBees--Flower-Recognition-System-"
       />
-
+      <ProjectCard 
+        title="CineScope" 
+        main="A movie information website that contains movie lists, such as popular, upcoming, and top rated. It utilized real-time data using API calls. Also contains movie details page complete with a unique chatbot." 
+        image="https://i.postimg.cc/jSYyYNHn/cine.jpg" 
+        demoLink="https://cine-scope-phi.vercel.app/"
+        githubLink="https://github.com/SahilChhatbar/CineScope-ReactJs"
+      />
     </div>
   </div>
   )
