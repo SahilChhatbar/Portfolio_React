afabccca73c3be8d7df7ce76caf676ced6d9bcb8
SahilChhatbar <sahilchhatbar7@gmail.com>
2024-11-05 10:12:06 +0530

    fef

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
     <ProjectCard 
         title="SamacharSetu" 
         main="A news website with a translation feature, completely responsive. Developed using React and Bootstrap." 
-        image="https://i.postimg.cc/tCc51Yxh/Screenshot-674.png" 
+        image="https://i.postimg.cc/XYM5SF5D/Screenshot-674.png" 
         demoLink="https://samacharsetu.vercel.app/"
         githubLink="https://github.com/SahilChhatbar/SamacharSetu"
       />
